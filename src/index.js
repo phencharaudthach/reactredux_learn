36da3ac306abd33392bfc53b3751afd431bc2d41
phencharaudthach <53065868+phencharaudthach@users.noreply.github.com>
2020-07-18 21:12:50 -0400

    how redux works with store, action, reducer, dispatch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// STORE -> GLOBALIZED STATE
+
+//ACTION INCREMENT; function that returns an object
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+// REDUCER -> how your actions transform from one state to the next (modify the store)
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH -> execute the action to the reducer
+store.dispatch(increment());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
